d549e6d12d71706fef86d42ffad52d4f121eb7db
luping <lupingxue@sohu-inc.com>
2021-08-21 18:49:31 +0800

    添加策略参数配置

--- src/main/java/com/huobi/constant/Constants.java
+++ src/main/java/com/huobi/constant/Constants.java
@@ -12,6 +12,7 @@ public class Constants {
     public static final String WX_PUSHER_TOKEN = "";
     public static final String WX_PUSHER_URL = "http://wxpusher.zjiecode.com/api/send/message";
 
+    /////////////////////////高频//////////////////////////////////
     public static final Double HIGH_RATIO = 0.8;
     public static final Double MEDIUM_RATIO = 0.1;
     public static final Double LOW_RATIO = 0.1;
@@ -20,7 +21,7 @@ public class Constants {
 
     public static final double HIGH_RANGE = 30;   // 0-30%
     public static final double MEDIUM_RANGE = 50; // 30-50%
-    public static final double LOW_RANGE = 70;   // 50-70%
+    public static final double LOW_RANGE = 70;   // 50-70%   <100
 
     // 策略区间内下单次数
     public static final double HIGH_COUNT = 20;
@@ -29,8 +30,40 @@ public class Constants {
 
     //offset 止盈点,  涨 2% 就卖掉.
     public static final BigDecimal SELL_OFFSET = new BigDecimal("1.02");
-
-    //自编订单号prefix
-    public static final String CLIENT_ID_PREFIX = "LUPING";
+    ///////////////////////////////////////////////////////////
+    /////////////////////////稳健//////////////////////////////////
+//    public static final Double HIGH_RATIO = 0.3;
+//    public static final Double MEDIUM_RATIO = 0.6;
+//    public static final Double LOW_RATIO = 0.1;
+//    //策略区间
+//    public static final double HIGH_RANGE = 10;   // 0-10%
+//    public static final double MEDIUM_RANGE = 40; // 10-40%
+//    public static final double LOW_RANGE = 50;   // 40-50%   <100
+//
+//    // 策略区间内下单次数
+//    public static final double HIGH_COUNT = 5;
+//    public static final double MEDIUM_COUNT = 10;
+//    public static final double LOW_COUNT = 2;
+//
+//    //offset 止盈点,  涨 2% 就卖掉.
+//    public static final BigDecimal SELL_OFFSET = new BigDecimal("1.15");
+    ///////////////////////////////////////////////////////////
+    /////////////////////////保守//////////////////////////////////
+//    public static final Double HIGH_RATIO = 0.3;
+//    public static final Double MEDIUM_RATIO = 0.3;
+//    public static final Double LOW_RATIO = 0.4;
+//    //策略区间
+//    public static final double HIGH_RANGE = 20;   // 0-20%
+//    public static final double MEDIUM_RANGE = 40; // 20-40%
+//    public static final double LOW_RANGE = 80;   // 40-80%   <100
+//
+//    // 策略区间内下单次数
+//    public static final double HIGH_COUNT = 4;  //5%下一次单
+//    public static final double MEDIUM_COUNT = 4; //10% 下一次单
+//    public static final double LOW_COUNT = 10;
+//
+//    //offset 止盈点,  涨 2% 就卖掉.
+//    public static final BigDecimal SELL_OFFSET = new BigDecimal("1.2");
+    ///////////////////////////////////////////////////////////
 
 }
