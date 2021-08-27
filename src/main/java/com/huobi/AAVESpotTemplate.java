fe8d1a0163a3670f4b52a704162c4b0c980f0a1f
luping <Xluping_S@hotmail.com>
2021-08-28 00:19:50 +0800

    刚才提交错了

--- src/main/java/com/huobi/AAVESpotTemplate.java
+++ src/main/java/com/huobi/AAVESpotTemplate.java
@@ -199,8 +199,7 @@ public class AAVESpotTemplate implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
-//        priceListener();
-        System.out.println(" -- SpotTemplate.execute -- ");
+        priceListener();
     }
 
     /**
--- src/main/java/com/huobi/SpotTemplate.java
+++ src/main/java/com/huobi/SpotTemplate.java
@@ -199,8 +199,7 @@ public class SpotTemplate implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
-//        priceListener();
-        System.out.println(" -- SpotTemplate.execute -- ");
+        priceListener();
     }
 
     /**
