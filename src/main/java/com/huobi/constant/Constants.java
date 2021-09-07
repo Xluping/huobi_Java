81b31bdce8479fd2387330c99efde2de6f3dfe13
lupingSS <90199165+lupingSS@users.noreply.github.com>
2021-09-08 00:27:43 +0800

    1. 高频 2. 稳健 3. 保守 对应3个jar

--- src/main/java/com/huobi/constant/Constants.java
+++ src/main/java/com/huobi/constant/Constants.java
@@ -13,54 +13,58 @@ public class Constants {
     public static final String WX_PUSHER_URL = "http://wxpusher.zjiecode.com/api/send/message";
 
     /////////////////////////高频//////////////////////////////////
-    public static final Double HIGH_RATIO_1 = 0.8;
-    public static final Double MEDIUM_RATIO_1 = 0.1;
-    public static final Double LOW_RATIO_1 = 0.1;
+    public static final Double HIGH_RATIO_1 = 0.6;
+    public static final Double MEDIUM_RATIO_1 = 0.2;
+    public static final Double LOW_RATIO_1 = 0.2;
 
-    //策略区间
+    //策略区间 30%以内
 
-    public static final double HIGH_RANGE_1 = 30;   // 0-30%
-    public static final double MEDIUM_RANGE_1 = 50; // 30-50%
-    public static final double LOW_RANGE_1 = 70;   // 50-70%   <100
+    public static final double HIGH_RANGE_1 = 10;   // 0-10%
+    public static final double MEDIUM_RANGE_1 = 20; // 10-20%
+    public static final double LOW_RANGE_1 = 30;   // 20-30%   <100
 
     // 策略区间内下单次数
-    public static final double HIGH_COUNT_1 = 60; // 0.5%
-    public static final double MEDIUM_COUNT_1 = 20; // 1%
-    public static final double LOW_COUNT_1 = 5; // 4%
+    public static final double HIGH_COUNT_1 = 40; // 0.25%
+    public static final double MEDIUM_COUNT_1 = 20; // 0.5%
+    public static final double LOW_COUNT_1 = 10; // 1%
 
     //offset 止盈点,  涨 2% 就卖掉.
     public static final BigDecimal SELL_OFFSET_1 = new BigDecimal("1.02");
     ///////////////////////////////////////////////////////////
+
+
     /////////////////////////稳健//////////////////////////////////
-    public static final Double HIGH_RATIO_2 = 0.6;
-    public static final Double MEDIUM_RATIO_2 = 0.3;
-    public static final Double LOW_RATIO_2 = 0.1;
-    //策略区间
+    public static final Double HIGH_RATIO_2 = 0.3;
+    public static final Double MEDIUM_RATIO_2 = 0.5;
+    public static final Double LOW_RATIO_2 = 0.2;
+    //策略区间 40%以内
     public static final double HIGH_RANGE_2 = 10;   // 0-10%
-    public static final double MEDIUM_RANGE_2 = 40; // 10-40%
-    public static final double LOW_RANGE_2 = 70;   // 40-70%   <100
+    public static final double MEDIUM_RANGE_2 = 30; // 10-30%
+    public static final double LOW_RANGE_2 = 40;   // 30-40%   <100
 
     // 策略区间内下单次数
-    public static final double HIGH_COUNT_2 = 5;  //1%
-    public static final double MEDIUM_COUNT_2 = 15; //2%
-    public static final double LOW_COUNT_2 = 6;    //5%
+    public static final double HIGH_COUNT_2 = 10;  //1%
+    public static final double MEDIUM_COUNT_2 = 40; //0.5%
+    public static final double LOW_COUNT_2 = 10;    //1%
 
     //offset 止盈点,  涨 2% 就卖掉.
     public static final BigDecimal SELL_OFFSET_2 = new BigDecimal("1.05");
     ///////////////////////////////////////////////////////////
+
+
     /////////////////////////保守//////////////////////////////////
-    public static final Double HIGH_RATIO_3 = 0.6;
-    public static final Double MEDIUM_RATIO_3 = 0.3;
-    public static final Double LOW_RATIO_3 = 0.1;
-    //策略区间
-    public static final double HIGH_RANGE_3 = 20;   // 0-20%
-    public static final double MEDIUM_RANGE_3 = 40; // 20-40%
-    public static final double LOW_RANGE_3 = 80;   // 40-80%   <100
+    public static final Double HIGH_RATIO_3 = 0.2;
+    public static final Double MEDIUM_RATIO_3 = 0.2;
+    public static final Double LOW_RATIO_3 = 0.6;
+    //策略区间 30%以内
+    public static final double HIGH_RANGE_3 = 10;   // 0-10%
+    public static final double MEDIUM_RANGE_3 = 15; // 10-15%
+    public static final double LOW_RANGE_3 = 35;   // 15-35%   <100
 
     // 策略区间内下单次数
-    public static final double HIGH_COUNT_3 = 4;  //5%下一次单
-    public static final double MEDIUM_COUNT_3 = 4; //5% 下一次单
-    public static final double LOW_COUNT_3 = 5;   //8% 下一次单
+    public static final double HIGH_COUNT_3 = 10;  //1%下一次单
+    public static final double MEDIUM_COUNT_3 = 10; //0.5% 下一次单
+    public static final double LOW_COUNT_3 = 20;   //1% 下一次单
 
     //offset 止盈点,  涨 2% 就卖掉.
     public static final BigDecimal SELL_OFFSET_3 = new BigDecimal("1.1");
