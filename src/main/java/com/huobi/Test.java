a56c2c37593f5cc43eb40872be477569a076aa66
luping <Xluping_S@hotmail.com>
2021-09-13 11:00:11 +0800

    1.暂不保存之前的卖单 2.修改定时任务时间

---
+++ src/main/java/com/huobi/Test.java
@@ -0,0 +1,20 @@
+package com.huobi;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @program: huobi-client
+ * @package: com.huobi
+ * @author: Luping
+ * @create: 9/13/21 10:45 AM
+ */
+@Slf4j
+public class Test {
+    public static void main(String[] args) {
+        log.warn("====== Test-main: {warn} ======");
+        log.debug("====== Test-main: {debug} ======");
+        log.info("====== Test-main: {info} ======");
+        log.error("====== Test-main: {error} ======");
+
+    }
+}
