96669de683ea7384a695772c5d5f0a6f05d3e824
lupingSS <90199165+lupingSS@users.noreply.github.com>
2021-09-16 00:51:09 +0800

    sell 时, 容易产生误差, 如果出错,按照99%的币下单卖出

--- src/main/java/com/huobi/Test.java
+++ src/main/java/com/huobi/Test.java
@@ -3,6 +3,7 @@ package com.huobi;
 import com.huobi.client.GenericClient;
 import com.huobi.client.req.account.AccountBalanceRequest;
 import com.huobi.client.req.market.CandlestickRequest;
+import com.huobi.client.req.trade.CreateOrderRequest;
 import com.huobi.client.req.trade.OrderHistoryRequest;
 import com.huobi.client.req.trade.OrdersRequest;
 import com.huobi.constant.HuobiOptions;
@@ -18,6 +19,7 @@ import com.huobi.model.trade.Order;
 import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -32,7 +34,24 @@ public class Test {
     static long accountId = 14086863L;
 
     public static void main(String[] args) {
+        Spot spot = new Spot();
+        spot.setBaseCurrency("hc");
+        spot.setQuoteCurrency("usdt");
+        spot.setSymbol(spot.getBaseCurrency() + spot.getQuoteCurrency());
+        spot.setAccountId(accountId);
 
+        List<Symbol> symbolList = CurrentAPI.getApiInstance().getGenericClient().getSymbols();
+        symbolList.forEach(symbol -> {
+            if (symbol.getBaseCurrency().equalsIgnoreCase(spot.getBaseCurrency()) && symbol.getQuoteCurrency().equalsIgnoreCase(spot.getQuoteCurrency())) {
+                spot.setPricePrecision(symbol.getPricePrecision());
+                spot.setAmountPrecision(symbol.getAmountPrecision());
+                spot.setMinOrderValue(symbol.getMinOrderValue());
+                spot.setLimitOrderMinOrderAmt(symbol.getLimitOrderMinOrderAmt());
+                spot.setSellMarketMinOrderAmt(symbol.getSellMarketMinOrderAmt());
+            }
+        });
+
+        sell(1, spot, new BigDecimal("0.7946"), new BigDecimal("65.23577"), 1);
     }
 
     public void filter() {
@@ -62,4 +81,49 @@ public class Test {
 
 
     }
+
+    public static void sell(int currentStrategy, Spot spot, BigDecimal buyPrice, BigDecimal coinAmount, int type) {
+        // 计算卖出价格 buyPrice * (1+offset);
+        try {
+            BigDecimal sellPrice = null;
+            if (currentStrategy == 1) {
+                sellPrice = buyPrice.multiply(Constants.SELL_OFFSET_1);
+            } else if (currentStrategy == 2) {
+                sellPrice = buyPrice.multiply(Constants.SELL_OFFSET_2);
+            } else if (currentStrategy == 3) {
+                sellPrice = buyPrice.multiply(Constants.SELL_OFFSET_3);
+            }
+            //自定义订单号
+            String clientOrderId = spot.getSymbol() + System.nanoTime();
+            // 价格,币数 有严格的小数位限制
+            assert sellPrice != null;
+            sellPrice = sellPrice.setScale(spot.getPricePrecision(), RoundingMode.HALF_UP);
+            coinAmount = coinAmount.setScale(spot.getAmountPrecision(), RoundingMode.DOWN);
+            //最小下单量限制
+            BigDecimal orderAmount;
+            //最小下单量限制
+            if (coinAmount.compareTo(spot.getLimitOrderMinOrderAmt()) < 0) {
+                log.info("====== {}-{}-StrategyCommon: 按最小下单币数下单 SELL {} ======", spot.getSymbol(), currentStrategy, spot.getLimitOrderMinOrderAmt());
+                orderAmount = spot.getLimitOrderMinOrderAmt();
+            } else {
+                orderAmount = coinAmount;
+            }
+
+            spot.setOrderAmount(orderAmount);
+
+            log.info("====== {}-{}-StrategyCommon: SELL at: {},  clientOrderId: {}, orderAmount: {}, type: {} ======", spot.getSymbol(), currentStrategy, sellPrice.toString(), clientOrderId, orderAmount, type);
+            CreateOrderRequest sellRequest;
+            if (type == 1) {
+                sellRequest = CreateOrderRequest.spotSellLimit(spot.getAccountId(), clientOrderId, spot.getSymbol(), sellPrice, orderAmount);
+            } else {
+                sellRequest = CreateOrderRequest.spotSellMarket(spot.getAccountId(), clientOrderId, spot.getSymbol(), orderAmount);
+            }
+            CurrentAPI.getApiInstance().getTradeClient().createOrder(sellRequest);
+        } catch (Exception e) {
+            log.error("======StrategyCommon.sell : 卖出时发生异常 {}, 重新尝试下单 99% ======", e.getMessage());
+            sell(currentStrategy, spot, buyPrice, coinAmount.multiply(new BigDecimal("0.99")), type);
+        }
+
+    }
+
 }
